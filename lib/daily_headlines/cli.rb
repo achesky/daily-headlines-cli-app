6e85e839008e3c99381933e3c5105317d19ec57b
achesky <afc340@nyu.edu>
2017-03-03 16:19:02 -0500

    began building out 'fake' program to experiment with what interface i want

--- lib/daily_headlines/cli.rb
+++ lib/daily_headlines/cli.rb
@@ -4,7 +4,22 @@ class DailyHeadlines::CLI
 
   def call
     puts "Today's Headlines"
+    list_papers
+    menu
   end
+
+  def list_papers
+    puts <<-DOC.gsub /^\s*/, ''
+    1. New York Times
+    2. Wall Street Journal
+    DOC
+  end
+
+  def menu
+    puts "Enter the paper you would like to read:"
+  end
+
+
 
 end
 
